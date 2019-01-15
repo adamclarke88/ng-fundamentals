cf6584cb72da5202dccb609686660cc41f8a9374
Adam Clarke <adamclarke88@gmail.com>
2019-01-15 12:27:48 +0000

    added styles using ngClass - started services

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,13 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
         <div class="well hoverwell thumbnail">
         <h2>{{event.name}}</h2>
         <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
+        <div [ngClass]="getTimeColour()" [ngSwitch]="event?.time" >Time: {{event.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+            <span *ngSwitchDefault>(Normal Start)</span>
+        </div>
+
+
         <div>Price: \${{event.price}}</div>
             <div>
                 <span>Location: {{event.location.address}}</span>
@@ -16,6 +22,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
         </div>
     `,
     styles:[`
+        .bold { font-weight: bold;}
+        .green { color: #003300 !important}
+        .red { color: red !important}
+        .blue { color: blue !important}
+        .thumbnail {min-height: 210px;}
         .pad-left {margin-left: 10px;}
         .well div { color: #bbb;}
     `]
@@ -25,5 +36,15 @@ export class EventThumbnailComponent{
     @Input() event:any
     @Output() eventClick = new EventEmitter()
 
+    // getStartTimeClass() {
+    //     var isEarlyStart = this.event && this.event.time === '8:00 am'
+    //     return {green: isEarlyStart, bold: isEarlyStart}
+    // }
 
+    getTimeColour() {
+        if (this.event.time === '8:00 am')
+        return 'green bold'
+        else if (this.event.time === '10:00 am')
+        return 'red bold'
+    }
 }

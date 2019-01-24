76713157c1bb47f0433c4d56a76d126fdea09ae6
Adam Clarke <adamclarke88@gmail.com>
2019-01-24 17:06:46 +0000

    added content projection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {
   EventDetailsComponent,
   CreateEventComponent,
   CreateSessionComponent,
+  SessionListComponent,
   EventRouteActivator
 
 } from './events/index'
@@ -18,6 +19,7 @@ import { EventsAppComponent } from './events-app.component';
 import { TestComponent } from './test/test.component';
 import { NavBarComponent } from './nav/navbar.component'
 import { ToastrService } from './common/toastr.service'
+import { CollapsibleWellComponent } from './common/collapsible-well.component'
 import { appRoutes } from './routes'
 import { Error404Component } from './errors/404.component';
 import { AuthService } from './user/auth.service'
@@ -40,6 +42,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     TestComponent,
     CreateEventComponent,
     CreateSessionComponent,
+    SessionListComponent,
+    CollapsibleWellComponent,
     Error404Component
   ],
   providers: [
